c856079b4a0163dc645bd430339940c9d454640d
simondlevy <simon.d.levy@gmail.com>
2023-09-24 22:02:59 -0400

    =More i2cdev

--- examples/ReadDistanceI2CDev/readDistance.cpp
+++ examples/ReadDistanceI2CDev/readDistance.cpp
@@ -4,6 +4,31 @@
 
 int main(int argc, char ** argv)
 {
+    static VL53L1X sensor;
+
+    sensor.begin();
+
+    while (true) {
+
+        sensor.startRanging(); 
+
+        while (true) {
+            uint8_t dataReady = 0;
+            sensor.checkForDataReady(&dataReady);
+            if (dataReady) {
+                break;
+            }
+        }
+
+        uint16_t distance = 0;
+        sensor.getDistance(&distance);
+
+        sensor.clearInterrupt();
+
+        sensor.stopRanging();
+
+        printf("Distance(mm): %d\n", distance);
+    }
 
     return 0;
 }
