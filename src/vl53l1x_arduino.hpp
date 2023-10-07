0fb53d15e1f296a88ed0e3d1e51ec9f7d19f0715
simondlevy <simon.d.levy@gmail.com>
2023-10-07 15:36:49 -0400

    Added header comments

--- src/vl53l1x_arduino.hpp
+++ src/vl53l1x_arduino.hpp
@@ -1,5 +1,38 @@
 #pragma once
 
+/*
+   Arduino support for VL53L1X
+
+   Copyright (c) 2023, Simon D. Levy
+   All Rights Reserved
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+   3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include "vl53l1x.hpp"
 
 #include <Wire.h>
@@ -61,7 +94,7 @@ VL53L1X::error_t VL53L1X::read_bytes(void * device, const uint16_t rgstr,
         i++;
     }
 
-    return 0;
+    return ERROR_NONE;
 }
 
 VL53L1X::error_t VL53L1X::write_bytes(void * device, const uint16_t rgstr, 
@@ -79,7 +112,8 @@ VL53L1X::error_t VL53L1X::write_bytes(void * device, const uint16_t rgstr,
         twoWire->write(data[i]);
     }
     twoWire->endTransmission(true);
-    return 0;
+
+    return ERROR_NONE;
 }
 
 void VL53L1X::delay_msec(const uint32_t msec)
